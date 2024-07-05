6e7f12e25b45d4121990599683d1bdfdbc8c41ab
supermabur <supermabur@gmail.com>
2024-07-05 16:32:07 +0700

    asdasd

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +12,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::resource('/', 'homeController');
